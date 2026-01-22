ebaae006a7aca4756b96871beba95b6097344579
Vishal <vishalq2478@gmail.com>
2026-01-22 12:59:21 +0530

    secure cors for production

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: [
+        "https://second-brain-frontend-tan.vercel.app/"
+    ],
+    credentials: true
+}));
+
 
 app.use(authRoutes);
 app.use(userContentRoutes);
